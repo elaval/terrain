6243b89d4b1efb09f9218df9463c21b322a3b17d
Ernesto Laval <ernesto.laval@gmail.com>
2018-05-04 18:43:00 -0300

    Cleanup the code

--- src/app/views/map/map.component.ts
+++ src/app/views/map/map.component.ts
@@ -13,7 +13,9 @@ import * as d3 from 'd3';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  
+  margin = {
+    top:50
+  }
   area;
   distance;
 
@@ -39,41 +41,52 @@ export class MapComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-
+    const windowSize = document.body.getBoundingClientRect().height;
+    d3.select("div[leaflet]").style("height", `${windowSize-this.margin.top}px`);
   }
 
   onMapReady(map: Map) {
-    console.log("MAP");
 
+    // The map is ready, we will configure some additional layers & drawing options
+    this.configureStartupLayers(map);
+    this.retsoreSavedLayers(map);
+    this.configureEventHandlers(map)
+  }
+
+  /**
+   * Set layers & controls on the map
+   */
+  configureStartupLayers(map) {
     this.drawnItems = L.featureGroup().addTo(map);
-    const googleLayer = new L.TileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
-      attribution: 'google'
-    });
 
-    const Layer = new L.TileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'satellite'
-    });
+    // Configure control layers (panel that allows to choose the base layer)
+    const openStreetMap = L.tileLayer(
+      'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 30
+    })
 
-    
-
-
-    const osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    const osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-    const osm = L.tileLayer(osmUrl, { maxZoom: 30, attribution: osmAttrib });
     const mapbox = L.tileLayer(
       'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWxhdmFsIiwiYSI6ImNqZ3JzdzhlMjA0OWkyd24xMGh2Nmp3ZjIifQ.LF8Unpt13Tmea5l6YyANRg', {
           tileSize: 512,
           zoomOffset: -1,
-          attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          maxZoom: 30
+    });
+
+    const google = new L.TileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
+      attribution: 'google',
+      maxZoom: 30
     });
 
     L.control.layers(
       {
-        'street': osm.addTo(map),
-        "sat": mapbox
+        'street': openStreetMap.addTo(map),
+        "sat": mapbox,
+        "google" : google
+
       },
-      {},
-      //{'drawlayer': this.drawnItems },
+      {},//{'drawlayer': this.drawnItems },
       { position: 'bottomleft', collapsed: false }
     ).addTo(map);
 
@@ -115,17 +128,13 @@ export class MapComponent implements OnInit {
       'draw': drawOptions,
       'edit': editOptions
     };
+  }
 
-    // Add layers that have been saved
-    this.shapesService.restoreLayers()
-    .then((layers:any[]) => {
-      layers.forEach(layer => {
-        layer.addTo(this.drawnItems);
-      });
-      this.updateMetrics();
-    })
-
-
+  /**
+   * Handle events (when items are created, edited, deleted, ...)
+   * @param map 
+   */
+  configureEventHandlers(map) {
     map.on(L.Draw.Event.CREATED,  (event: LeafletEvent) => {
       var layer = event['layer'];
 
@@ -151,32 +160,52 @@ export class MapComponent implements OnInit {
     }
     
     map.on({
+      // Used as configuration helpers
       'zoomend': displayPos,
       'moveend': displayPos,
     })
-
-
   }
 
+  /**
+   * Get back to life shapes that have been saved
+   * @param map 
+   */
+  retsoreSavedLayers(map) {
+    // Add layers that have been saved
+    this.shapesService.restoreLayers()
+    .then((layers:any[]) => {
+      layers.forEach(layer => {
+        layer.addTo(this.drawnItems);
+      });
+      this.updateMetrics();
+    })
+  }
+
+  /**
+   * Calculate metrics (area & distance) for current rectangles, polygons and polylines
+   */
   updateMetrics() {
     const layers = this.drawnItems.getLayers();
     const metrics = this.shapesService.calculateMetrics(layers);
 
     // We need to use zone.run, to make sure that Angular updates the properties 
     this.zone.run(() => {
+      // Transform are units into non-metric units
       this.area = L.GeometryUtil.readableArea(metrics.area, false);
+
+      // Transform distance into non-metric units
       this.distance = L.GeometryUtil['readableDistance'](metrics.length, "yard");
     })
 
   }
 
-
+  /**
+   * Save existing shapes into persistent storage (localStorage for the demo)
+   */
   saveLayers() {
     const layers = this.drawnItems.getLayers();
     this.shapesService.saveLayers(layers);
   }
-
-
 
 }
 
